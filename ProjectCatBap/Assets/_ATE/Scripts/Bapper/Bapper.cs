c86a26e756bf543bfe1bbd7c100277d5e0facc3f
ArmoredThirteen <6456070+ArmoredThirteen@users.noreply.github.com>
2020-03-02 14:22:28 -0800

    Added bapper controller, removed dead code and concept description from bapper, set up scene to have two bappers

--- ProjectCatBap/Assets/_ATE/Scripts/Bapper/Bapper.cs
+++ ProjectCatBap/Assets/_ATE/Scripts/Bapper/Bapper.cs
@@ -2,22 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace ATE
+namespace ATE.Bapper
 {
-    // Concept #1
-    // Receive click in location
-    // Move arm so hand lines up with bapLoc
-    // When near end, begin rotating paw back
-    // At location, paw is fully rotated
-    // Either time out and reset arm to start location
-    //    or bap down and to the side
-    // Either animation completes and then arm resets
-    //    or receive click and perform another bap
-    //    (repeat)
-    // When arm begins reset (while still extended)
-    //    Unlock other to be active bapper
-
-    // Concept #2 (current chosen)
     // Click/hold at location
     // Arm moves to location
     // Baps happen continually
@@ -29,7 +15,6 @@ namespace ATE
 	public class Bapper : MonoBehaviour
     {
         public Transform faceToObject = null;
-
         public Transform armToMove = null;
 
         public float maxReachDistance = 5;
@@ -38,26 +23,29 @@ namespace ATE
         public float speedMoveToStartPos = 1;
 
         public float speedRotateFaceMouse = 1;
-        public float speedRotateToStartRot = 1;
 
 
         private Vector2 startPos;
-        private Quaternion startRot;
+
+
+        public bool IsActive
+        {
+            get; set;
+        }
 
 
         private void Awake()
         {
             startPos = transform.position;
-            startRot = transform.rotation;
         }
 
         private void FixedUpdate()
         {
-            if (Input.GetMouseButton (0))
+            if (IsActive && Input.GetMouseButton (0))
                 BapTowardMouse (speedMoveToPosition * Time.deltaTime, speedRotateFaceMouse * Time.deltaTime);
             //else if (Input.GetMouseButton (1))
             else
-                ResetBapper (speedMoveToStartPos * Time.deltaTime, speedRotateToStartRot * Time.deltaTime);
+                ResetBapper (speedMoveToStartPos * Time.deltaTime);
 
             // Rotate to face smooth mouse
             Vector2 rotDir = ((Vector2)faceToObject.position - (Vector2)transform.position).normalized;
@@ -74,11 +62,12 @@ namespace ATE
             armToMove.localPosition += Vector3.up * moveActual;
         }
 
-        private void ResetBapper(float moveSpeed, float rotateSpeed)
+        private void ResetBapper(float moveSpeed)
         {
             if (Vector3.Distance (armToMove.position, startPos) <= 0.05)
                 return;
 
+            //TODO: Can get pushed too far and then quickly and automatically drifts away from startPos
             // Move back to starting position
             float moveActual = Mathf.Min (moveSpeed, Vector3.Distance (armToMove.position, startPos));
             armToMove.localPosition += Vector3.down * moveActual;
---
+++ ProjectCatBap/Assets/_ATE/Scripts/Bapper/BapperController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ATE.Bapper
+{
+	public class BapperController : MonoBehaviour
+	{
+        public Bapper bapperLeft = null;
+        public Bapper bapperRight = null;
+
+        
+        public void Start()
+        {
+            ActivateRight ();
+        }
+
+        public void Update()
+        {
+            if (Input.GetMouseButtonDown (0))
+                ToggleActive ();
+        }
+
+
+        private void ToggleActive()
+        {
+            bapperLeft.IsActive = !bapperLeft.IsActive;
+            bapperRight.IsActive = !bapperRight.IsActive;
+        }
+
+        private void ActivateLeft()
+        {
+            bapperLeft.IsActive = true;
+            bapperRight.IsActive = false;
+        }
+
+        private void ActivateRight()
+        {
+            bapperLeft.IsActive = false;
+            bapperRight.IsActive = true;
+        }
+
+    }
+}
